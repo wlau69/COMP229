ad7d578716c5714a9701756343f82a508752d636
Amber <wingyan_999@hotmail.com>
2022-01-14 01:19:52 -0500

    commit

---
+++ Closures/index.js
@@ -0,0 +1,21 @@
+function parent() {
+   const message = 'Hello World';
+
+   function child() {
+      alert (message);
+   }
+
+   return child;
+}
+
+const childFN = parent();
+childFN();
+
+// function parent() {
+//    const message = 'Hello World';
+//    function child() {
+//       alert (message);
+//    }
+//    child();
+// }
+// parent();
